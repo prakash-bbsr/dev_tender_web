5b5985577be87cdc2b5f14d417389ecb3216f350
prakash <pcn2007nayak@gmail.com>
2026-05-09 16:38:35 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,44 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
-import './App.css'
+import './index.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-     Welcome to Hello world 
+    <div className="navbar bg-base-300">
+  <div className="flex-1">
+    <a className="btn btn-ghost text-xl">Dev Tender</a>
+  </div>
+  <div className="flex-none gap-2">
+    <div className="form-control">      
+    </div>
+    <div className="dropdown dropdown-end">
+      <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
+        <div className="w-10 rounded-full">
+          <img
+            alt="Tailwind CSS Navbar component"
+            src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
+        </div>
+      </div>
+      <ul
+        tabIndex={0}
+        className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
+        <li>
+          <a className="justify-between">
+            Profile
+            <span className="badge">New</span>
+          </a>
+        </li>
+        <li><a>Settings</a></li>
+        <li><a>Logout</a></li>
+      </ul>
+    </div>
+  </div>
+</div>
+     <h1 className='text-3xl font-bold underline'>Welcome to Hello world </h1>
     </>
   )
 }

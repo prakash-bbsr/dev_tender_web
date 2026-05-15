c1ad42189012f061210567f5150e7ef0d129d097
prakash <pcn2007nayak@gmail.com>
2026-05-15 17:15:51 +0530

    Signup

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -5,8 +5,11 @@ import { useDispatch } from "react-redux";
 import { addUser } from "../utils/userSlice";
 import { useNavigate } from "react-router-dom";
 const Login = () => {
+  const [firstName,setFirstName] = useState("");
+  const [lastName,setLastName] = useState("");
   const [emailId,setEmailId] = useState("");
   const [password,setPassword] = useState("");
+  const [isLoginForm,setIsLoginForm] = useState(false);
   //Error handler
   const [error,setError] = useState("");
   const navigate = useNavigate();
@@ -26,10 +29,48 @@ const Login = () => {
         setError(err?.response?.data?.message || "Invalid credentials");        
       }
   }
+  const handelSignUp = async()=>{
+    try{
+      const res = await axios.post(BASE_URL+"/signUp",{
+        firstName,lastName,emailId,password
+      },{withCredentials:true});
+      dispatch(addUser(res.data.data));
+      return navigate("/profile");
+    }catch{err}{
+      setError(err?.response?.data || "Something went wrong");
+    }
+  }
   return <div className="min-h-screen flex items-center justify-center -mt-20">
     <div className="card bg-primary text-primary-content w-96">
       <div className="card-body">
-        <h2 className="card-title">Login </h2>        
+        <h2 className="card-title">{isLoginForm ? "Login" :"Sign UP"} </h2>  
+      {!isLoginForm && <><label className="input input-bordered flex items-center gap-2">
+            <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 16 16"
+                fill="currentColor"
+                className="h-4 w-4 opacity-70">
+                <path d="M8 8a3 3 0 1 0-0-6 3 3 0 0 0 0 6Zm-5 6a5 5 0 1 1 10 0H3Z" />
+            </svg>
+
+            <input
+                type="text"
+                value={firstName}
+                className="grow"
+                placeholder="First Name"
+                onChange={(e) => setFirstName(e.target.value)}
+            />
+            </label>
+            <label className="input input-bordered flex items-center gap-2">
+            <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 16 16"
+                fill="currentColor"
+                className="h-4 w-4 opacity-70">
+                <path d="M8 8a3 3 0 1 0-0-6 3 3 0 0 0 0 6Zm-5 6a5 5 0 1 1 10 0H3Z" />
+            </svg>
+            <input type="text" value={lastName} className="grow"  placeholder="Last name" onChange={(e) => setLastName(e.target.value)} />
+      </label> </>}        
       <label className="input input-bordered flex items-center gap-2">
         <svg
           xmlns="http://www.w3.org/2000/svg"
@@ -61,9 +102,14 @@ const Login = () => {
             {error}
           </p>
         )}
+        
         <div className="card-actions justify-center pt-5">
-          <button className="btn justify-center" onClick={handelLogin}>Login</button>
+          <button className="btn justify-center" onClick={isLoginForm ? handelLogin : handelSignUp}> {isLoginForm ? "Login" : "Sign Up"} </button>
         </div>
+        <p className="text-center py-2 cursor-pointer text-blue-500 hover:underline" onClick={()=>setIsLoginForm((value) => !value)}>{isLoginForm
+                      ? "New user ? Signup Here"
+                      : "Existing User? Login Here"
+          }</p>
       </div>
     </div>
   </div>
